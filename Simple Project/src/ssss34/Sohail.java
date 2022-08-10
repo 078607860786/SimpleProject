5d399640b755c4fcbaa86e42a4db1f5cdfb8f4f5
Muhammad Faizan Khalil <faizan.eccountant@gmail.com>
2022-08-10 16:26:46 +0500

    Sohail Sagar

---
+++ Simple Project/src/ssss34/Sohail.java
@@ -0,0 +1,14 @@
+package ssss34;
+
+public class Sohail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Sohail Sagar is Great Person");
+		System.out.print("Sohail Sagar is Great Person");
+		System.out.print("Sohail Sagar is Great Person");
+
+
+	}
+
+}
